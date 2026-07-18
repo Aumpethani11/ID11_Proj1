3e1c3726b9a870ce07900ef74bb01f4581f5d6e4
Vaibhav Patel <vhp19012005@gmail.com>
2026-07-18 12:35:27 +0530

    firebase

--- DAY-01_IMPACT/src/firebase.js
+++ DAY-01_IMPACT/src/firebase.js
@@ -3,12 +3,13 @@ import "firebase/compat/auth";
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-YrIFHIZQcFdYouvVix2O5d6YVjIqxg4",
-  authDomain: "authentication-d4695.firebaseapp.com",
-  projectId: "authentication-d4695",
-  storageBucket: "authentication-d4695.firebasestorage.app",
-  messagingSenderId: "1006779223139",
-  appId: "1:1006779223139:web:1ac6c4059893c0a76e7c56",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 
 };
 
